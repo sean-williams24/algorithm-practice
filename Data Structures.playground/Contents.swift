3b3444f808cfc5e07c64c837b624a3ba4b334b42
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2020-06-29 18:03:21 +0100

    Trees and Traversal

--- Data Structures.playground/Contents.swift
+++ Data Structures.playground/Contents.swift
@@ -1,5 +1,97 @@
 import UIKit
 
+// Trees
+
+class Node {
+    
+    var value: Int
+    var left: Node?
+    var right: Node?
+    
+    init(value: Int) {
+        self.value = value
+    }
+}
+
+class BinaryTree {
+    
+    var root: Node
+    
+    init(rootValue: Int) {
+        self.root = Node(value: rootValue)
+    }
+    
+    // Return true if the value is in the tree, otherwise return false
+    func search(_ value: Int) -> Bool {
+        return preorderSearch(root, value: value)
+    }
+    
+    // Return a string containing all tree nodes as they are visited in a pre-order traversal.
+    func printTree() -> String {
+        return String((preorderPrint(root, traverse: "\(root.value)-")?.dropLast(1))!)
+    }
+    
+    
+    // Helper method - use to create a recursive search solution.
+    func preorderSearch(_ start: Node?, value: Int) -> Bool {
+        if start?.value == value {
+            return true
+        }
+        
+        var result = false
+        if start?.left != nil {
+               result = preorderSearch(start?.left, value: value)
+            }
+        
+        if result == false {
+            if start?.right != nil {
+               result = preorderSearch(start?.right, value: value)
+            }
+        }
+        
+        return result
+    }
+    
+    // Helper method - use to construct a string representing the preordered nodes
+    func preorderPrint(_ start: Node?, traverse:  String) -> String? {
+        var str = traverse
+        
+        if start?.left != nil {
+            str.append("\(start?.left?.value ?? 80)-")
+            str = preorderPrint(start?.left, traverse: str)!
+            
+            if start?.right != nil {
+                str.append("\(start?.right?.value ?? 90)-")
+                 str = preorderPrint(start?.right, traverse: str)!
+            }
+        }
+        return str
+    }
+}
+
+// Test cases
+// Set up tree
+let tree = BinaryTree(rootValue: 1)
+tree.root.left = Node(value: 2)
+tree.root.right = Node(value: 3)
+tree.root.left?.left = Node(value: 4)
+tree.root.left?.right = Node(value: 5)
+
+// Test search
+print(tree.search(4)) // Should be true
+print(tree.search(6)) // Should be false
+print(tree.search(1))
+print(tree.search(2))
+print(tree.search(3))
+print(tree.search(5))
+
+// Test printTree
+print(tree.printTree()) // Should be 1-2-4-5-3
+
+
+
+
+
 //MARK: - Hashing
 
 class HashTable {
