50b67bbb1ce63e79d63a5e292b61cafeaa66b63a
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2020-06-30 17:24:51 +0100

    Graphs

--- Data Structures.playground/Contents.swift
+++ Data Structures.playground/Contents.swift
@@ -1,18 +1,192 @@
 import UIKit
 
+// MARK: - Graphs
+
+struct Node {
+    
+    var value: Int?
+    var edges: [Edge]
+    
+    init(value: Int?) {
+        self.value = value
+        self.edges = []
+    }
+}
+
+struct Edge {
+    
+    var value: Int?
+    var nodeFrom: Node?
+    var nodeTo: Node?
+    
+    init(value: Int?, nodeFrom: Node?, nodeTo: Node?) {
+        self.value = value
+        self.nodeFrom = nodeFrom
+        self.nodeTo = nodeTo
+    }
+}
+
+class Graph {
+    
+    var nodes: [Node]
+    var edges: [Edge]
+    
+    init(nodes: [Node], edges: [Edge]) {
+        self.nodes = nodes
+        self.edges = edges
+    }
+    
+    // Don't return an array of edge objects!
+    // Return an array of arrays where inner arrays look like
+    // (Edge Value, From Node Value, To Node Value)
+    func getEdgeList() -> [[Int]] {
+        var edgeList = [[Int]]()
+        
+        for edge in edges {
+            var innerList: [Int] = []
+            innerList.append(edge.value ?? 0)
+            innerList.append(edge.nodeFrom?.value ?? 0)
+            innerList.append(edge.nodeTo?.value ?? 0)
+            edgeList.append(innerList)
+        }
+        
+        return edgeList
+    }
+    
+    // Don't return any Node or Edge objects!
+    // You'll return a 3D array
+    // The indices of the outer list represent "from" nodes.
+    // Each section in the array will store an array of arrays where the inner-most arrays look like
+    // (To Node Value, Edge Value)
+    // Should be [[], [(2, 100), (3, 101), (4, 102)], [], [(4, 103)], []]
+    func getAdjacencyList() -> [[[Int]]] {
+        let max = getMaxIndex()
+        var adjacencyList = [[[Int]]]()
+        
+        for a in 0...max {
+            var b: [[Int]] = []
+            for edge in edges {
+                if edge.nodeFrom?.value == a {
+                    var c: [Int] = []
+                    c.append(edge.nodeTo?.value ?? 99)
+                    c.append(edge.value ?? 999)
+                    b.append(c)
+                }
+            }
+            adjacencyList.insert(b, at: a)
+            
+            if !nodes.contains(where: {$0.value == a}) {
+                adjacencyList.insert(b, at: a)
+            }
+        }
+        return adjacencyList
+    }
+    
+    // Return a matrix, or 2D array.
+    // Row numbers represent from nodes.
+    // Column numbers represent to nodes.
+    // Store the edge values in each spot, and a 0 if no edge exists.
+    // Should be [[0, 0, 0, 0, 0], [0, 0, 100, 101, 102], [0, 0, 0, 0, 0], [0, 0, 0, 0, 103], [0, 0, 0, 0, 0]]
+    func getAdjacencyMatrix() -> [[Int]] {
+        let max = getMaxIndex()
+        var adjacencyMatrix = [[Int]]()
+
+        for a in 0...max {
+            var inner = [Int]()
+
+            for b in 0...max {
+                inner.append(0)
+
+                for edge in edges {
+                    if edge.nodeFrom?.value == a && edge.nodeTo?.value == b {
+                        inner.remove(at: b)
+                        inner.append(edge.value ?? 0)
+                    }
+                }
+            }
+            adjacencyMatrix.append(inner)
+        }
+        return adjacencyMatrix
+    }
+    
+    // Helper to be used with adjacency list and adjacency matrix
+    // max node value determines the size of the array
+    func getMaxIndex() -> Int {
+        var maxIndex = 0
+        
+        for node in nodes {
+            if node.value! > maxIndex {
+                maxIndex = node.value!
+            }
+        }
+        
+        return maxIndex
+    }
+    
+    // creates a node with a given value and inserts it into the graph
+    func insertNodeWithValue(_ value: Int) {
+        let newNode = Node(value: value)
+        nodes.append(newNode)
+    }
+    
+    // inserts an edge between the "to" and "from" nodes with the specified values
+    func insertEdgeWithValue(_ newEdgeValue: Int, nodeFromValue: Int, nodeToValue: Int) {
+        var fromFound: Node? = nil
+        var toFound: Node? = nil
+        
+        for node in nodes {
+            if nodeFromValue == node.value {
+                fromFound = node
+            }
+            if nodeToValue == node.value {
+                toFound = node
+            }
+        }
+        
+        if fromFound == nil {
+            fromFound = Node(value: nodeFromValue)
+            nodes.append(fromFound!)
+        }
+        if toFound == nil {
+            toFound = Node(value: nodeToValue)
+            nodes.append(toFound!)
+        }
+        
+        let newEdge = Edge(value: newEdgeValue, nodeFrom: fromFound, nodeTo: toFound)
+        fromFound?.edges.append(newEdge)
+        toFound?.edges.append(newEdge)
+        edges.append(newEdge)
+    }
+}
+
+// Test cases
+let node = Node(value: 1)
+let graph = Graph(nodes: [node], edges: [])
+graph.insertEdgeWithValue(100, nodeFromValue: 1, nodeToValue: 2)
+graph.insertEdgeWithValue(101, nodeFromValue: 1, nodeToValue: 3)
+graph.insertEdgeWithValue(102, nodeFromValue: 1, nodeToValue: 4)
+graph.insertEdgeWithValue(103, nodeFromValue: 3, nodeToValue: 4)
+
+print(graph.getEdgeList()) // Should be [(100, 1, 2), (101, 1, 3), (102, 1, 4), (103, 3, 4)]
+print(graph.getAdjacencyList()) // Should be [[], [(2, 100), (3, 101), (4, 102)], [], [(4, 103)], []]
+print(graph.getAdjacencyMatrix()) // Should be [[0, 0, 0, 0, 0], [0, 0, 100, 101, 102], [0, 0, 0, 0, 0], [0, 0, 0, 0, 103], [0, 0, 0, 0, 0]]
+
+
+
+
 
 // MARK: - Binary Search Trees
 
-class Node {
-    
-    var value: Int
-    var left: Node?
-    var right: Node?
-    
-    init(value: Int) {
-        self.value = value
-    }
-}
+//class Node {
+//
+//    var value: Int
+//    var left: Node?
+//    var right: Node?
+//
+//    init(value: Int) {
+//        self.value = value
+//    }
+//}
 class BST {
     
     var root: Node
@@ -79,7 +253,7 @@ class BST {
             
             if start?.right != nil {
                 str.append("\(start?.right?.value ?? 90)-")
-                 str = preorderPrint(start?.right, traverse: str)!
+                str = preorderPrint(start?.right, traverse: str)!
             }
         }
         return str
@@ -132,24 +306,24 @@ class BinaryTree {
     
     
     // Helper method - use to create a recursive search solution.
-//    func preorderSearch(_ start: Node?, value: Int) -> Bool {
-//        if start?.value == value {
-//            return true
-//        }
-//
-//        var result = false
-//        if start?.left != nil {
-//               result = preorderSearch(start?.left, value: value)
-//            }
-//
-//        if result == false {
-//            if start?.right != nil {
-//               result = preorderSearch(start?.right, value: value)
-//            }
-//        }
-//
-//        return result
-//    }
+    //    func preorderSearch(_ start: Node?, value: Int) -> Bool {
+    //        if start?.value == value {
+    //            return true
+    //        }
+    //
+    //        var result = false
+    //        if start?.left != nil {
+    //               result = preorderSearch(start?.left, value: value)
+    //            }
+    //
+    //        if result == false {
+    //            if start?.right != nil {
+    //               result = preorderSearch(start?.right, value: value)
+    //            }
+    //        }
+    //
+    //        return result
+    //    }
     
     func preorderSearch(_ start: Node?, value: Int) -> Bool {
         if let start = start {
@@ -173,7 +347,7 @@ class BinaryTree {
             
             if start?.right != nil {
                 str.append("\(start?.right?.value ?? 90)-")
-                 str = preorderPrint(start?.right, traverse: str)!
+                str = preorderPrint(start?.right, traverse: str)!
             }
         }
         return str
@@ -282,8 +456,8 @@ print(locations)
 
 if let nAmerica = locations["North America"]?["USA"] {
     for city in nAmerica.sorted() {
-     print(city)
- }
+        print(city)
+    }
     
 }
 
@@ -295,7 +469,7 @@ for city in locations["North America"]!["USA"]!.sorted() {
 //MARK: - Quick Sort / Recursion
 
 func quicksort(_ input: inout [Int], low: Int, high: Int) -> [Int] {
-//    let last = input.count - 1
+    //    let last = input.count - 1
     // check the base case: low is less than high
     if low == high {
         return input
@@ -310,7 +484,7 @@ func quicksort(_ input: inout [Int], low: Int, high: Int) -> [Int] {
         input.append(lowMover)
         input.insert(highMover, at: low)
         quicksort(&input, low: low, high: high - 1)
-
+        
     } else {
         quicksort(&input, low: low + 1, high: high)
     }
@@ -318,7 +492,7 @@ func quicksort(_ input: inout [Int], low: Int, high: Int) -> [Int] {
     
     // make the recursive call with the new high and low values
     quicksort(&input, low: low, high: high - 1)
-
+    
     return input
 }
 
@@ -348,7 +522,7 @@ print(quicksort(&testArray, low: 0, high: testArray.count - 1))
 
 
 func getFib(_ position: Int) -> Int {
-
+    
     if position == 0 || position == 1 {
         return position
     }
